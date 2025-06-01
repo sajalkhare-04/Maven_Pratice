261a68696b72a801ec84c7852ed056b61cc129ad
Sajal <sajalkhare0497@gmail.com>
2025-06-01 05:18:54 +0000

    Maven project is created

---
+++ src/main/java/pratice_maven/App.java
@@ -0,0 +1,10 @@
+package pratice_maven;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World this build is made for maven pratice");
+    }
+}
